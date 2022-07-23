61fb133c5789160a0ab5ccbfc1dd4299dff0853d
Tamarawillis44 <techietl444@gmail.com>
2022-07-22 22:27:53 -0400

    Initial Commit

---
+++ Dragon BallAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dragon BallAnimation
+//
+//  Created by Tamara laraque on 7/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dragon BallAnimation/Dragon_BallAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dragon_BallAnimationApp.swift
+//  Dragon BallAnimation
+//
+//  Created by Tamara laraque on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Dragon_BallAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
